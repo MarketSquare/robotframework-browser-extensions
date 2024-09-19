26e3f90d63140d292e1958b0a0b9622b43f8f8cd
Lukas Boekenoogen <lukas.boekenoogen@>
2024-09-19 16:35:39 +0200

    Update

--- Playwright-page-methods/playwright_page_method.js
+++ Playwright-page-methods/playwright_page_method.js
@@ -1,7 +1,17 @@
 async function playwrightPageMethod(page_method, page, logger) {
     const playwright_function = '(page.' + page_method + ')';
     logger("Executing Playwright Page method: " + page_method)
-    return eval(playwright_function);
+    return await eval(playwright_function);
+}
+
+function convertLocatorToBrowser(playwright_locator, logger) {
+    if (typeof playwright_locator == "object") {
+        const browser_locator = playwright_locator['_selector']
+        logger("Locator converted to : " + browser_locator)
+        return browser_locator;
+    } else {
+        throw new Error("convertPlaywrightLocatorToBrowser: playwright_locator is not an object");
+    }
 }
 
 playwrightPageMethod.rfdoc = `
@@ -13,5 +23,17 @@ Example
 | Playwright Page Method  getByRole('link', { name: 'Get started' }).click()
 `
 
+convertLocatorToBrowser.rfdoc = `
+Converts a returned PlaywrightPageMethod locator to a BrowserLibrary locator.
+
+Parameters: playwright_locator : (object) Playwright locator object.
+
+Example:
+|  \${pw_locator}    Playwright Page Method    getByRole('link', { name: 'Get started' })
+|  \${bl_locator}    Convert Locator To Browser    {pw_locator}
+|  Click  \${bl_locator}
+`
+
 exports.__esModule = true;
 exports.playwrightPageMethod = playwrightPageMethod;
+exports.convertLocatorToBrowser = convertLocatorToBrowser;
--- Playwright-page-methods/playwright_page_method.robot
+++ Playwright-page-methods/playwright_page_method.robot
@@ -7,10 +7,12 @@ Native Playwright
     New Browser    headless=False
     New Context
     New Page    https://playwright.dev
-    ${btn_txt}    Playwright Page Method    getByRole('link', { name: 'Get started' }).innerText()
+    ${btn_txt}        Playwright Page Method    getByRole('link', { name: 'Get started' }).innerText()
     Log To Console    btn txt= ${btn_txt}
     Playwright Page Method    getByRole('link', { name: 'Get started' }).click()
     Playwright Page Method    getByLabel('Search').click()
     Playwright Page Method    getByLabel('Search').nth(1).type('locators')
-    Playwright Page Method    getByRole('link', { name: 'Filtering Locators​ Locators' }).click()
-    Sleep    5s
+    ${pw_locator}    Playwright Page Method    getByRole('link', { name: 'FrameLocator' })
+    ${clickme}       Convert Locator To Browser    playwright_locator=${pw_locator}
+    Click            ${clickme}
+    Sleep            5s
